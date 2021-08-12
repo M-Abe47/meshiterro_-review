afb623bb62f1519a2fd63f541ca29826bfd110e8
M-Abe47 <abuzhengze5@gmail.com>
2021-08-12 08:24:28 +0000

    画像投稿追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a88d4b5334d6496bbddd035705f91d00030be30193a2d1f6cae666f3f51a45b0a9a28ec3a6c2001aeb855f709ba05ef8719408daa51d8bbf7f0fb47c30c9f294'
